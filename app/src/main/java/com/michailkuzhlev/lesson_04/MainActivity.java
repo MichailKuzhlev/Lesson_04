e1abbf4aba562b1c84fbe3d1a8553a78313331ac
michailkuzhlev <michailkuzhlev@yandex.ru>
2022-02-05 14:37:22 +0700

    created themes for my three RadioGroup

--- app/src/main/java/com/michailkuzhlev/lesson_04/MainActivity.java
+++ app/src/main/java/com/michailkuzhlev/lesson_04/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.my_themes_Space_Gray);
         setContentView(R.layout.activity_main);
     }
 }
