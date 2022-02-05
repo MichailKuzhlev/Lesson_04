69c467bc264fe79e4059f06f2afd6a2a26b683ec
michailkuzhlev <michailkuzhlev@yandex.ru>
2022-02-05 16:16:02 +0700

    Created MyButtonStyle + FonFamily + background themes

--- app/src/main/java/com/michailkuzhlev/lesson_04/MainActivity.java
+++ app/src/main/java/com/michailkuzhlev/lesson_04/MainActivity.java
@@ -3,13 +3,45 @@ package com.michailkuzhlev.lesson_04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(R.style.my_themes_Space_Gray);
+        setTheme(MyApp.currentTheme);
         setContentView(R.layout.activity_main);
+
+        ((RadioButton) findViewById(R.id.materialRadioButtonSpaceGray)).setOnClickListener(this);
+        ((RadioButton) findViewById(R.id.materialRadioButtonGreen)).setOnClickListener(this);
+        ((RadioButton) findViewById(R.id.materialRadioButtonBlue)).setOnClickListener(this);
+        ((RadioButton) findViewById(R.id.materialRadioButtonRed)).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.materialRadioButtonSpaceGray:{
+                MyApp.currentTheme=R.style.my_themes_Space_Gray;
+                break;
+            }
+            case R.id.materialRadioButtonGreen:{
+                MyApp.currentTheme=R.style.my_themes_green;
+
+                break;
+            }
+            case R.id.materialRadioButtonBlue:{
+                MyApp.currentTheme=R.style.my_themes_blue;
+                break;
+            }
+            case R.id.materialRadioButtonRed:{
+                MyApp.currentTheme=R.style.my_themes_Red;
+                break;
+            }
+        }
+        recreate();
+
     }
 }
